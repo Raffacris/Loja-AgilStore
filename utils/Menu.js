6177e1bf684bc7d4da6a5b511dfc7ec1c75213c6
Rafaela Cristina <rafacris87@gmail.com>
2026-01-06 11:49:19 -0300

    Adicionando menu interativo no terminal

---
+++ utils/Menu.js
@@ -0,0 +1,13 @@
+function showMenu() {
+  console.log(`
+🛒 AGILSTORE - MENU
+1 - Adicionar produto
+2 - Listar produtos
+3 - Atualizar produto
+4 - Excluir produto
+5 - Buscar produto
+0 - Sair
+`);
+}
+
+module.exports = showMenu;
