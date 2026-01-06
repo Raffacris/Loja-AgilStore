8f3b712c0aa8d34d87d95e1d4ced98a9782404d7
Rafaela Cristina <rafacris87@gmail.com>
2026-01-06 11:40:53 -0300

    Criando o modelo do produto

---
+++ models/Product.js
@@ -0,0 +1,10 @@
+class Product{
+    constructor(nome,categoria,quantidade,preco){
+        this.id= null;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+}
+module.exports = Product;
